47fff75cc651f658a002646226fe6c0ba4d84288
ndizigiye <ndiarmand11@yahoo.fr>
2013-04-20 19:33:05 +0200

    'new-changes-20-04-2013'

--- app.js
+++ app.js
@@ -20,28 +20,67 @@ io.sockets.on('connection', function(socket) {
 
 	socket.on('user', function(name, time) {
 		var user = new User(name, socket.id, time);
-		console
-				.log("Name:---" + user.getName() + "Time: ----"
-						+ user.getTime());
-
-		var html = "<html>" + "<body>" + "The user said: " + user.getName()
-				+ "</body>" + "</html>";
-		var appjs = 'var express = require("express");'
-				+ 'var fs = require("fs");'
-				+ 'var module = require("./lib/user.js");'
-				+ 'var app = express();' + 'var http = require("http");'
-				+ 'var server = http.createServer(app);'
-				+ 'var io = require("socket.io").listen(server);';
-		fs.writeFile('Games/index.html', html, function(err) {
+		
+		var html_mobile = "<html>"+'\n'
+				+"<script src='/socket.io/socket.io.js'></script>"+'\n'
+				+"<script>"
+				+"function "+name+"(){"+'\n'
+				+"var socket = io.connect('http://' + window.location.host + '/');"+'\n'
+				+"socket.emit('"+name+"','Hello world');"+'\n'
+				+"}"+'\n'
+				+"</script>"+'\n'
+				+ "<body>"+'\n'
+				+"<button onclick='"+name+"()'>"+name+"</button>"+'\n'
+				+ "</body>"+'\n'
+				+ "</html>";
+		
+		var html_monitor = '<html>'+'\n'
+			+'<script src="/socket.io/socket.io.js"></script>'+'\n'
+			+'<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>'+'\n'
+			+'<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>'+'\n'
+			+'<script src="http://infosupport.jit.su/lib/prettyPhoto/prettyphoto.js"></script>'+'\n'
+			+'<script>'+'\n'
+			+'var socket = io.connect("http://" + window.location.host + "/");'+'\n'
+			+'socket.on("'+name+'", function(msg){'+'\n'
+			+'$("#inline").text(msg);'+'\n'
+			+'$.prettyPhoto.open("#inline","'+name+'","'+name+'");'+'\n'
+			+'});'+'\n'
+			+'</script>'+'\n'
+			+'<body>'+'\n'
+			+'<div id="inline">'+'\n'
+			+'</div>'+'\n'
+			+'</body>'+'\n'
+			+'</html>';
+		
+		var appjs = 'var express = require("express");'+'\n'
+				+ 'var fs = require("fs");'+'\n'
+				+ 'var app = express();' + 'var http = require("http");'+'\n'
+				+ 'var server = http.createServer(app);'+'\n'
+				+ 'var io = require("socket.io").listen(server);'+'\n'
+				+'server.listen(8080 || process.env.PORT);'+'\n'
+				+'app.use(express.static(__dirname + "/"));'+'\n'
+				+'io.sockets.on("connection", function(socket) {'+'\n'
+				+'socket.on("'+name+'", function(msg) {'+'\n'
+				+'socket.broadcast.emit("'+name+'",msg);'+'\n'
+				+'});'+'\n'
+				+'});';
+		
+		fs.writeFile('Games/index.html', html_mobile, function(err) {
 			if (err)
 				throw err;
-			console.log('It\'s saved!');
+			console.log('mobile saved!');
 		});
-
+		
+		fs.writeFile('Games/monitor.html', html_monitor, function(err) {
+			if (err)
+				throw err;
+			console.log('monitor saved!');
+		});
+		
 		fs.writeFile('Games/app.js', appjs, function(err) {
 			if (err)
 				throw err;
-			console.log('It\'s saved!');
+			console.log('app.js saved!');
 		});
 	});
 
