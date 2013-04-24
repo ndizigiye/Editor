dd6c518d3326811059a573241e8abe5c8a97fe44
ndizigiye <ndiarmand11@yahoo.fr>
2013-04-24 17:23:38 +0200

    new changes 24-4

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(express.static(__dirname + '/'));
 function writeFile(location,html){
 	fs.writeFile(location, html, function(err) {
 		if (err)
-			throw err;
+			console.log(err);
 		console.log('file saved!');
 	});
 }
@@ -44,7 +44,7 @@ io.sockets.on('connection', function(socket) {
     	var monitor_html = new Html();
     	
     	var mobilehtml = mobile_html.addHeader() + mobile_html.addJs() + mobile_html.addFooter();
-    	var monitorhtml = monitor_html.addHeader() + monitor_html.addJs() + monitor_html.addFooter();
+    	var monitorhtml = monitor_html.addHeader() + monitor_html.addJs() +monitor_html.addListener()+ monitor_html.addFooter();
     	
     	var mobile_location = './Games/'+id+'/index.html';
     	var monitor_location = './Games/'+id+'/monitor.html';
@@ -52,6 +52,11 @@ io.sockets.on('connection', function(socket) {
     	writeFile(mobile_location  , mobilehtml);
     	writeFile(monitor_location  , monitorhtml);
     	
+    });
+    
+    socket.on('save',function(path,html){
+    	writeFile(path, html);
+    	console.log(path+ " ..."+html);
     });
 
 	socket.on('disconnect', function() {
