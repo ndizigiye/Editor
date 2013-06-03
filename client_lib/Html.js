0e4636206e31704c70ce25a6fc81972ce2270e56
ndizigiye <ndiarmand11@yahoo.fr>
2013-06-03 18:56:16 +0200

    versie 1.0 ppt

--- client_lib/Html.js
+++ client_lib/Html.js
@@ -89,7 +89,7 @@ Html.prototype.addButton = function(name,type,data) {
 };
 
 Html.prototype.addPresentationButton = function(name,data) {
-	var parentDiv  = ["<div data-role='controlgroup' data-type='horizontal'>","</div>"];
+	var parentDiv  = ["<div class='buttongroup'>","</div>"];
 	var delButton = '<a href="#" data-role="button" data-icon="delete" data-iconpos="notext" data-corners="true" data-shadow="true" data-iconshadow="true" data-wrapperels="span" data-theme="c" title="Delete" class="ui-btn ui-btn-up-c ui-shadow ui-btn-corner-all ui-btn-icon-notext buttondel"><span class="ui-btn-inner"><span class="ui-btn-text">Delete</span><span class="ui-icon ui-icon-delete ui-icon-shadow">&nbsp;</span></span></a>'+'\n';
 	var html =  '<div data-role="controlgroup" data-type="horizontal" class="ui-corner-all ui-controlgroup ui-controlgroup-horizontal" aria-disabled="false" data-disabled="false" data-shadow="false" data-corners="true" data-exclude-invisible="true" data-mini="false" data-init-selector=":jqmData(role=\'controlgroup\')">'+'\n'
 				+'<div class="ui-controlgroup-controls">'+'\n'
@@ -97,10 +97,11 @@ Html.prototype.addPresentationButton = function(name,data) {
 				+'<a href="#" data-role="button" data-corners="true" data-shadow="true" data-iconshadow="true" data-wrapperels="span" data-theme="c" class="ui-btn ui-shadow ui-btn-corner-all ui-first-child ui-btn-up-c"><span class="ui-btn-inner"><span class="ui-btn-text">Open</span></span></a>'+'\n'
 				+'<a href="#" data-role="button" data-corners="true" data-shadow="true" data-iconshadow="true" data-wrapperels="span" data-theme="c" class="ui-btn ui-shadow ui-btn-corner-all ui-first-child ui-btn-up-c"><span class="ui-btn-inner"><span class="ui-btn-text">N</span></span></a>'+'\n'
 				+'</div>'+'\n'
-				+delButton
 				+'</div>'+'\n'
+	var delButton = '<a href="#" data-role="button" data-icon="delete" data-iconpos="notext" data-corners="true" data-shadow="true" data-iconshadow="true" data-wrapperels="span" data-theme="c" title="Delete" class="ui-btn ui-btn-up-c ui-shadow ui-btn-corner-all ui-btn-icon-notext buttondel"><span class="ui-btn-inner"><span class="ui-btn-text">Delete</span><span class="ui-icon ui-icon-delete ui-icon-shadow">&nbsp;</span></span></a>'+'\n';
 	console.log('created');
-	return html+'\n';	
+	return parentDiv[0]+'\n'+html+delButton+parentDiv[1]+'\n';	
+
 };
 
 Html.prototype.addDiv = function(id,content) {
